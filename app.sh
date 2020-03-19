b4c71045ada55d95de262624c97ad92d6f1d1082
karwootang-gft <karwoo.tang@gft.com>
2020-03-19 19:35:51 +0000

    moved FLASK_

--- app.sh
+++ app.sh
@@ -1,2 +1,3 @@
 LOGLEVEL=debug
+FLASK_RUN_PORT="${PORT}"
 CONFIGFILE="config/gcp_development.py" DEBUG="True" gunicorn --workers=5 --bind=0.0.0.0:5000 --log-level="${LOGLEVEL}" --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' app:connex_app
