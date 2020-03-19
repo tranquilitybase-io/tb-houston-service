86a4ac6808a7ab043a1bdf5d558bb629be1bf01e
karwootang-gft <karwoo.tang@gft.com>
2020-03-19 19:37:27 +0000

    FLASK_RUN_PORT in app.sh and app_dev.sh

--- app.sh
+++ app.sh
@@ -1,3 +1,4 @@
 LOGLEVEL=debug
+PORT=3000
 FLASK_RUN_PORT="${PORT}"
-CONFIGFILE="config/gcp_development.py" DEBUG="True" gunicorn --workers=5 --bind=0.0.0.0:5000 --log-level="${LOGLEVEL}" --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' app:connex_app
+CONFIGFILE="config/gcp_development.py" DEBUG="True" gunicorn --workers=5 --bind=0.0.0.0:3000 --log-level="${LOGLEVEL}" --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' app:connex_app
