d8ff7686ce0f288ad6a6795b80d1a5957201f1e8
Fábio Andrey Salles <fyss@gft.com>
2020-03-26 17:34:02 -0300

    MySQL 5.7 Container

--- app.sh
+++ app.sh
@@ -8,5 +8,6 @@ if [ ! -z "${1}" ]; then
 fi
 echo "Using port: ${PORT}"
 FLASK_RUN_PORT=${PORT}
+echo "Using DB server IP: ${DB_SERVER_IP}"
 
 CONFIGFILE="config/gcp_development.py" DEBUG="True" gunicorn --workers=${NUMBER_OF_WORKERS} --bind=0.0.0.0:${PORT} --log-level="${LOGLEVEL}" --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' app:connex_app
