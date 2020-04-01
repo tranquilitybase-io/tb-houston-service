fefeead16dcc6f16e14cee0867315476049aa073
karwootang-gft <karwoo.tang@gft.com>
2020-04-01 16:02:50 +0100

    fix application and solution post

--- models.py
+++ models.py
@@ -5,32 +5,39 @@ import json
 
 class ModelTools():
 
-    def get_utc_epoch(self):
+    @staticmethod
+    def get_utc_epoch():
         return datetime.utcnow().strftime('%s')
 
-    def get_utc_timestamp(self):
+    @staticmethod
+    def get_utc_timestamp():
         return datetime.utcnow().strftime(("%Y-%m-%d %H:%M:%S"))
 
-    def get_timestamp(self):
+    @staticmethod
+    def get_timestamp():
         return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
+    @staticmethod
     def datetime_as_string(dt):
         if dt is None:
             return datetime.utcnow().strftime(("%Y-%m-%d %H:%M:%S"))
         else:
             return dt.strftime("%Y-%m-%d %H:%M:%S")
 
+    @staticmethod
     def load_json_array(s):
         try:
             return json.loads(s)
         except Exception as e:
             return []
 
+    @staticmethod
     def json_dump(obj):
         #return json.dumps(obj, indent=2, sort_keys=True)
         return json.dumps(obj)
 
     # simple redact function, used prior to logging
+    @staticmethod
     def redact_dict(my_dict):
         new_dict = my_dict.copy()
         new_dict['username'] = "XXXXX"
@@ -186,6 +193,10 @@ class ApplicationSchema(ma.ModelSchema):
         model = Application
         sqla_session = db.session
 
+    solutionId = fields.Int()
+    activatorId = fields.Int()
+
+
 # Solutions
 class Solution(db.Model):
     __tablename__ = "solution"
@@ -229,17 +240,6 @@ class SolutionSchema(ma.ModelSchema):
             app.logger.warning(e)
         return out_data
 
-# class ApplicationActivatorSchema(ma.ModelSchema):
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-#
-#     class Meta:
-#         model = Application
-#         sqla_session = db.session
-#
-#     id = fields.Int()
-#     name = fields.Str()
-#     activators = fields.Nested('ActivatorSchema')
 
 # Team
 class Team(db.Model):
