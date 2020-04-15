b840741d515f5834162dc7cb59c81f297c2cf6e1
karwootang-gft <karwoo.tang@gft.com>
2020-04-15 19:07:20 +0100

    fix Codacy issue

--- app_docker.sh
+++ app_docker.sh
@@ -11,4 +11,4 @@ echo "Using port: ${PORT}"
 echo "Using DB server IP: ${DB_SERVER_IP}"
 echo "SH SQLALCHEMY_DATABASE_URI: ${SQLALCHEMY_DATABASE_URI}"
 
-SQLALCHEMY_DATABASE_URI="${SQLALCHEMY_DATABASE_URI}" DEBUG="True" gunicorn --workers=${NUMBER_OF_WORKERS} --bind=0.0.0.0:${PORT} --log-level="${LOGLEVEL}" --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' app:connex_app
+SQLALCHEMY_DATABASE_URI="${SQLALCHEMY_DATABASE_URI}" DEBUG="True" gunicorn --workers="${NUMBER_OF_WORKERS}" --bind="0.0.0.0:${PORT}" --log-level="${LOGLEVEL}" --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' app:connex_app
