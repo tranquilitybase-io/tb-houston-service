e236a580dec5d3f44b2c84b3d6c88b12c77e4d9e
karwootang-gft <karwoo.tang@gft.com>
2020-04-15 16:22:11 +0100

    fix issues raised by Codacy

--- app_dev.sh
+++ app_dev.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 LOGLEVEL=error
 NUMBER_OF_WORKERS=5
 PORT=3000
@@ -5,7 +6,5 @@ PORT=3000
 if [ ! -z "${1}" ]; then
 	PORT=${1}
 fi
-#echo "Using port: ${PORT}"
-FLASK_RUN_PORT="${PORT}"
 
 SQLALCHEMY_TRACK_MODIFICATIONS='False' SQLALCHEMY_ECHO='False' SQLALCHEMY_DATABASE_URI="${SQLALCHEMY_DATABASE_URI}" CONFIGFILE="config/local_development.py" DEBUG="${LOGLEVEL}" gunicorn --workers=${NUMBER_OF_WORKERS} --bind=0.0.0.0:${PORT} --log-level="${LOGLEVEL}" --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' app:connex_app
