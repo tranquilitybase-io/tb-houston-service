c2c2cf23987c7b843f14b1f9fb149fdea7175422
karwootang-gft <karwoo.tang@gft.com>
2020-04-27 09:53:19 +0100

    deal with codacy issue

--- tests/test_cd.py
+++ tests/test_cd.py
@@ -9,17 +9,17 @@ HOUSTON_SERVICE_URL=os.environ['HOUSTON_SERVICE_URL']
 url = f"http://{HOUSTON_SERVICE_URL}/api/keyValues/cd/"
     
 # Additional headers.
-headers = {'Content-Type': 'application/json' } 
+headers = {'Content-Type': 'application/json' }
 
-key = 'test/' 
+key = 'test/'
            
 def test_post():
 
     #Test POST Then GET
     # Body
     payload = {'key': 'test', 'value': 'test-value'}
-    # convert dict to json by json.dumps() for body data. 
-    resp = requests.post(url, headers=headers, data=json.dumps(payload,indent=4))       
+    # convert dict to json by json.dumps() for body data.
+    resp = requests.post(url, headers=headers, data=json.dumps(payload,indent=4))
     
     # Validate response headers and body contents, e.g. status code.
     resp_json = resp.json()
@@ -30,8 +30,8 @@ def test_post():
     #Get Request to check Post has created item as expected
     resp = requests.get(url+'test', headers=headers) 
     resp_json = resp.json()
-    resp_headers = resp.headers    
-    #Validate response 
+    resp_headers = resp.headers
+    #Validate response
     assert resp.status_code == 200
     assert resp_json['key'] == 'test'
     assert resp_json['value'] == 'test-value'
@@ -56,7 +56,7 @@ def test_put():
     assert resp_json['key'] == 'test'
     assert resp_json['value'] == 'new-test-value'
 
-  
+
 def test_delete():
 
     #Test Delete Then GET
@@ -65,11 +65,11 @@ def test_delete():
 
 def test_get_error():
 
-    resp = requests.get(url+'test', headers=headers) 
-    resp_json = resp.json()
+    resp = requests.get(url+'test', headers=headers)
+    #resp_json = resp.json()
     assert resp.status_code == 404
 
 def test_get_all():
 
-    resp = requests.get(url, headers=headers)  
+    resp = requests.get(url, headers=headers)
     assert resp.status_code == 200
