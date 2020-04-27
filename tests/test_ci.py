10d15e113bbac328b6c4c805917f142728590eb2
karwootang-gft <karwoo.tang@gft.com>
2020-04-27 12:30:08 +0100

    codacy

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -10,21 +10,21 @@ url = f"http://{HOUSTON_SERVICE_URL}/api/keyValues/ci/"
 # Additional headers.
 headers = {'Content-Type': 'application/json' }
 
-key = 'test/'
+#key = 'test/'
            
 def test_post():
 
     #Test POST Then GET
     # Body
-    payload = {'key': 'test', 'value': 'test-value'}
-    # convert dict to json by json.dumps() for body data. 
+    payload = {'key': 'test', 'value': 'test-post-value'}
+    # convert dict to json by json.dumps() for body data.
     resp = requests.post(url, headers=headers, data=json.dumps(payload,indent=4))
     
     # Validate response headers and body contents, e.g. status code.
     resp_json = resp.json()
     assert resp.status_code == 201
     assert resp_json['key'] == 'test'
-    assert resp_json['value'] == 'test-value'
+    assert resp_json['value'] == 'test-post-value'
     
     #Get Request to check Post has created item as expected
     resp = requests.get(url+'test', headers=headers) 
@@ -33,7 +33,7 @@ def test_post():
     #Validate response
     assert resp.status_code == 200
     assert resp_json['key'] == 'test'
-    assert resp_json['value'] == 'test-value'
+    assert resp_json['value'] == 'test-post-value'
     assert resp_headers['content-type'] == 'application/json'
 
 
@@ -47,9 +47,9 @@ def test_put():
     assert resp.status_code == 200
 
     #Get Request to get updated values
-    resp = requests.get(url+'test', headers=headers) 
+    resp = requests.get(url+'test', headers=headers)
     resp_json = resp.json()
-   
+
     #Validate response body for updated values
     assert resp.status_code == 200
     assert resp_json['key'] == 'test'
@@ -59,7 +59,7 @@ def test_put():
 def test_delete():
 
     #Test Delete Then GET
-    resp = requests.delete(url+'test', headers=headers) 
+    resp = requests.delete(url+'test', headers=headers)
     assert resp.status_code == 200
 
 def test_delete_error():
