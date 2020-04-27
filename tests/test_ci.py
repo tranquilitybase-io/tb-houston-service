2ecc5bf6416d9bd70c50be3b37ac82c98726c79e
karwootang-gft <karwoo.tang@gft.com>
2020-04-27 09:55:12 +0100

    deal with codacy issue

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -8,9 +8,9 @@ HOUSTON_SERVICE_URL=os.environ['HOUSTON_SERVICE_URL']
 url = f"http://{HOUSTON_SERVICE_URL}/api/keyValues/ci/"
     
 # Additional headers.
-headers = {'Content-Type': 'application/json' } 
+headers = {'Content-Type': 'application/json' }
 
-key = 'test/' 
+key = 'test/'
            
 def test_post():
 
@@ -18,7 +18,7 @@ def test_post():
     # Body
     payload = {'key': 'test', 'value': 'test-value'}
     # convert dict to json by json.dumps() for body data. 
-    resp = requests.post(url, headers=headers, data=json.dumps(payload,indent=4))       
+    resp = requests.post(url, headers=headers, data=json.dumps(payload,indent=4))
     
     # Validate response headers and body contents, e.g. status code.
     resp_json = resp.json()
@@ -30,7 +30,7 @@ def test_post():
     resp = requests.get(url+'test', headers=headers) 
     resp_json = resp.json()
     resp_headers = resp.headers
-    #Validate response 
+    #Validate response
     assert resp.status_code == 200
     assert resp_json['key'] == 'test'
     assert resp_json['value'] == 'test-value'
@@ -43,7 +43,7 @@ def test_put():
     newpayload = {'key': 'test', 'value': 'new-test-value'}
     resp = requests.put(url+'test', headers=headers, data=json.dumps(newpayload,indent=4))
    
-    #Validate update/Put response 
+    #Validate update/Put response
     assert resp.status_code == 200
 
     #Get Request to get updated values
@@ -55,7 +55,7 @@ def test_put():
     assert resp_json['key'] == 'test'
     assert resp_json['value'] == 'new-test-value'
 
-  
+
 def test_delete():
 
     #Test Delete Then GET
@@ -65,16 +65,16 @@ def test_delete():
 def test_delete_error():
 
     #Test Delete for an non existing item
-    resp = requests.delete(url+'test', headers=headers) 
+    resp = requests.delete(url+'test', headers=headers)
     assert resp.status_code == 404
 
 def test_get_error():
 
-    resp = requests.get(url+'test', headers=headers) 
-    resp_json = resp.json()
+    resp = requests.get(url+'test', headers=headers)
+    #resp_json = resp.json()
     assert resp.status_code == 404
 
 def test_get_all():
 
-    resp = requests.get(url, headers=headers)  
+    resp = requests.get(url, headers=headers)
     assert resp.status_code == 200
