d8b94c94f79bbe1f8b117cf59a17659fe9400199
karwootang-gft <karwoo.tang@gft.com>
2020-04-28 15:19:02 +0100

    remove a test

--- tests/test_login.py
+++ tests/test_login.py
@@ -23,23 +23,3 @@ def test_login():
     resp_json = resp.json()
     # Validate Response
     assert resp.status_code == 401
-
-
-def test_login_pass():
-    HOUSTON_SERVICE_URL=os.environ['HOUSTON_SERVICE_URL']
-    url = f"http://{HOUSTON_SERVICE_URL}/api/login"
-
-    headers = {'Content-Type': 'application/json' } 
-
-    false = 1 == 0
-    payload = {
-    "username": "dev@your.company",
-    "password": "notapassword"
-    }
-
-    resp = requests.post(url, headers=headers, data=json.dumps(payload, indent=4))
-    resp_json = resp.json()
-    # Validate Response
-    assert resp.status_code == 401
-    print(resp_json['isAdmin'])
-
