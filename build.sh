a65ecbb0e28069f13bdee47bb8ecf26dc90daeab
karwootang-gft <karwoo.tang@gft.com>
2020-06-22 18:24:08 +0100

    codacy

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ tag=$(git for-each-ref --sort=-v:refname --count=1 --format '%(refname)'  refs/t
 echo "tag: ${tag}"
 if [[ ! -z $tag ]];
 then
-	docker build -t gcr.io/tranquility-base-images/tb-houston-service:${tag} .
-	docker push gcr.io/tranquility-base-images/tb-houston-service:${tag}
+	docker build -t gcr.io/tranquility-base-images/tb-houston-service:"${tag}" .
+	docker push gcr.io/tranquility-base-images/tb-houston-service:"${tag}"
 else
 	echo "Unable to find latest tag!"
 	exit 1
