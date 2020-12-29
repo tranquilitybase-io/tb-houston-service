f7afe3601d483c129cc97d8e82a91069ad05c056
Fabio Andrey Salles <fyss@gft.com>
2020-12-29 19:58:49 -0300

    Fixed #425 and some other structural changes

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
 FROM python:3.6
-RUN apt-get update && apt-get install -y --no-install-recommends default-mysql-client=1.0.5 dos2unix=7.4.0-1 \
+RUN apt-get update && apt-get install -y --no-install-recommends default-mysql-client=1.0.5 \
  && apt-get clean \
  && rm -rf /var/lib/apt/lists/*
-WORKDIR /srv
+WORKDIR /usr/src/app
 COPY requirements.txt .
 RUN pip install -r ./requirements.txt
 COPY . .
-RUN dos2unix app_docker.sh
-RUN ["chmod", "+x", "./app_docker.sh"]
-EXPOSE 3000
-CMD ["/bin/bash", "./app_docker.sh"]
+
+ENV NUMBER_OF_WORKERS=${NUMBER_OF_WORKERS:-5} \
+    PORT=${PORT:-3000}
+
+RUN adduser --disabled-password houston
+USER houston
+
+EXPOSE ${PORT}
+CMD gunicorn --workers="${NUMBER_OF_WORKERS}" --bind="0.0.0.0:${PORT}" app:connex_app
--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import os
 import logging
 import config
 
-# print("DEBUG: {}".format(os.environ['DEBUG']))
-# print("SQLALCHEMY_DATABASE_URI: {}".format(os.environ['SQLALCHEMY_DATABASE_URI']))
-
 # Get theapplication instance
 connex_app = config.connex_app
 connex_app.add_api("houston_service.yml", strict_validation=True)
@@ -15,7 +12,7 @@ gunicorn_logger = logging.getLogger("gunicorn.info")
 connex_app.app.logger.handlers = gunicorn_logger.handlers
 connex_app.app.logger.setLevel(gunicorn_logger.level)
 
-logging.basicConfig(level=int(os.environ.get("DEBUG")))
+logging.basicConfig(level=int(os.environ.get("LOGLEVEL") or 20))
 ch = logging.StreamHandler()
 formatter = logging.Formatter(
     "[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s",
